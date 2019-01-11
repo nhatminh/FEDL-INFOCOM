67552a5417cb7b4078a163399e4129e9308235b2
ArkShi <fshi3427@uni.sydney.edu.au.com>
2019-01-11 17:20:13 +1100

    extend the results tonon-iid data

--- linearFSVGR/venv/main_fed.py
+++ linearFSVGR/venv/main_fed.py
@@ -6,39 +6,43 @@ import tensorflow as tf
 from tqdm import tqdm
 import tensorboard
 from options import args_parser
-from sample_data import linear_data_generation, linear_iid, collect_data
+from sample_data import linear_iid_data_generation, linear_non_iid_data_generation, linear_iid, collect_data
 from model import ServerModel, ClientLocalModel, FederatedClientsModel
 import time
 if __name__ == '__main__':
     
     args = args_parser()
     args.gpu = -1  # -1 (CPU only) or GPU = 0
-    args.lr = 0.0000035
+    args.lr = 0.0000025
     args.ag_scalar = 1.0
     args.model = 'linear'
     args.dataset = 'ld'
     args.num_users = 3
-    args.epochs = 20  # numb of global iters
-    args.local_ep = 20  # numb of local iters
+    args.epochs = 50  # numb of global iters
+    args.local_ep = 50  # numb of local iters
     args.local_bs = 10  # Local Batch size (420 = full dataset size of a user)
     args.algorithm = 'fsvgr'
     args.lg_scalar = 1.0
-    args.iid = True
+    args.iid = False
     args.feature_dims = 10
     args.hidden_dims = 5
     print("dataset:", args.dataset, " num_users:", args.num_users, " epochs:", args.epochs, "local_ep:", args.local_ep)
     
     train_data, train_label = [], []
     dict_users = {}
-    if args.dataset == 'ld' and args.model == 'linear':
-        train_data, train_label = linear_data_generation('./data/ld_train.csv', args.feature_dims, 100)
-        train_label = np.expand_dims(train_label, axis=-1)
     if args.iid == True and args.model == 'linear':
+        train_data, train_label = linear_iid_data_generation('./data/ld_train_iid.csv', args.feature_dims, 100)
+        train_label = np.expand_dims(train_label, axis=-1)
         dict_users = linear_iid(len(train_data), args.num_users)
+    if not args.iid and args.model == 'linear':
+        train_data, train_label, dict_users = linear_non_iid_data_generation('./data/ld_train_non_iid.csv', args.feature_dims,
+                                                                             300, args.num_users)
+        train_label = np.expand_dims(train_label, axis=-1)
         
     if args.algorithm == 'fsvgr':
         tf.reset_default_graph()
         server = ServerModel(args.feature_dims, args.hidden_dims, 1, args.ag_scalar)
+        print(dict_users)
         clients = FederatedClientsModel(dict_users, args.feature_dims, args.hidden_dims, 1, train_data, train_label,
                                         args.local_ep, args.lr, args.lg_scalar, args.local_bs)
 
@@ -70,7 +74,7 @@ if __name__ == '__main__':
         plt.xlabel('global_epoch_num')
         plt.subplot(122)
         plt.plot(range(len(avg_loss)), avg_loss)
-        plt.savefig('./save/global_and_users_avg_loss_{}_global_epochs_{}_local_epochs.png'.format(args.epochs, args.local_ep))
+        plt.savefig('./save/{}_global_and_users_avg_loss_{}_global_epochs_{}_local_epochs.png'.format(args.iid, args.epochs, args.local_ep))
 
 
 
--- linearFSVGR/venv/sample_data.py
+++ linearFSVGR/venv/sample_data.py
@@ -3,7 +3,30 @@ import csv
 np.random.seed(0)
 
 
-def linear_data_generation(path, size, length):
+def linear_non_iid_data_generation(path, size, length, num_users):
+    each_user_data_num = length // num_users
+    dict_users = {}
+    all_features = []
+    all_labels = []
+    for u in range(num_users):
+        features = np.random.rand(each_user_data_num, size) * (u + 1)
+        w = np.random.rand(size)
+        noise = (u + 1) / 10 * np.random.rand(each_user_data_num)
+        b = np.random.rand(1)
+        labels = np.add(np.add(np.dot(features, np.transpose(w)), b), np.transpose(noise))
+        all_features.append(features)
+        all_labels.append(labels)
+    all_features = np.concatenate(np.array(all_features), axis=0)
+    all_labels = np.concatenate(np.array(all_labels), axis=0)
+    with open(path, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(np.hstack((all_features, all_labels.reshape(length, 1))))
+    for u in range(num_users):
+        dict_users[u] = set(range(u * each_user_data_num, u * each_user_data_num + each_user_data_num))
+    return all_features, all_labels, dict_users
+
+
+def linear_iid_data_generation(path, size, length):
     features = np.random.rand(length, size)
     w = np.random.rand(size)
     b = np.random.rand(1)
