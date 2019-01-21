1433ba4f13a0125d651035ddaccc30ed5242fe2e
Fangzhou Shi <35002588+Ark0617@users.noreply.github.com>
2019-01-21 21:21:31 +1100

    Update FedNets.py

--- federated-learning-master/FedAvg/FedNets.py
+++ federated-learning-master/FedAvg/FedNets.py
@@ -31,16 +31,14 @@ class CNNMnist(nn.Module):
         super(CNNMnist, self).__init__()
         print("NN: CNNMnist is created")
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.bn1 = nn.BatchNorm2d(10)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.bn2 = nn.BatchNorm2d(20)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, args.num_classes)
 
     def forward(self, x):
-        x = self.bn1(F.relu(F.max_pool2d(self.conv1(x), 2)))
-        x = self.bn2(F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2)))
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
